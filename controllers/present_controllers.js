aa8eb8b3f8664179f64a41bd5992102d158467fc
Alex Griep <alexgriep@hotmail.com>
2020-09-28 17:00:52 -0500

    finished necessary CRUD controller

--- controllers/present_controllers.js
+++ controllers/present_controllers.js
@@ -10,17 +10,27 @@ module.exports = function(app) {
         })
     });
 
-    //POST (create new present)
-    // app.post("/api/presents/", (req, res) => {
-    //     db.create({
-    //         present_name: req.body.present_name,
-    //         price: req.body.price,
-    //         url: req.body.url
-    //     })
-    //     .then(result => {
-    //         res.send(result);
-    //     });
-    // })
+    // POST (create new present)
+    app.post("/api/presents/", (req, res) => {
+        db.Presents.create({
+            present_name: req.body.present_name,
+            price: req.body.price,
+            url: req.body.url
+        })
+        .then(result => {
+            res.send(result);
+        });
+    })
 
     //DELETE (delete a present)
+    app.delete("/api/presents/:id", (req, res) => {
+        db.Presents.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(result => {
+            res.json(result);
+        })
+    })
 }
